b73f1ebcc2a8f093b95599f0b0a6c77c641a6944
Abulaf1a <peterrazzell688@gmail.com>
2024-12-01 16:08:05 +0000

    feat: create C# project file

---
+++ advent-of-code-project/Day1.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace advent_of_code_project
+{
+    public class Day1()
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello world!"); 
+
+        }
+
+    }
+}
